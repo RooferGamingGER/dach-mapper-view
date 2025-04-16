4014704ea5dd31cc1d4a7054fce8c76a7e537de7
RooferGamingGER <58918203+RooferGamingGER@users.noreply.github.com>
2025-04-16 14:25:23 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import './leaflet.css'  // Add Leaflet CSS import
+import "leaflet/dist/leaflet.css";
+import "leaflet-draw/dist/leaflet.draw.css"; // ✅ DIES ist wichtig
 
 createRoot(document.getElementById("root")!).render(<App />);
